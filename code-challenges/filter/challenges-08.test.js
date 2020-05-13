cc9eded80d4238f96f9d3cd4848e911aad530780
MasonChance <mason.fryberger@gmail.com>
2020-05-13 15:04:16 -0700

    final question

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -81,6 +81,11 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+  const moreSlim = arr.filter((val) =>{
+   const realSlim = !forbiddenValues.includes(val);
+    return realSlim;
+  })
+  return moreSlim;
 };
 
 /* ------------------------------------------------------------------------------------------------
